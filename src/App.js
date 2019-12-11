60e3b882ba256a9923a46d98c622a639f857631e
jiaxin-MXX <13583487600@qq.com>
2019-12-11 18:15:00 +0800

    '.'

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import TabBarExample from './pages/tabBar'
 import 'assets/reset.css'
 import 'swiper/css/swiper.min.css'
 // import
+// w132
 function App() {
   // console.log(TabBarExample)
   return (
