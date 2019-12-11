ac34cd5d2e00b1a35dafc89f549ec88b0a633fe6
jiaxin-MXX <13583487600@qq.com>
2019-12-11 18:08:29 +0800

    'hai'

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import TabBarExample from './pages/tabBar'
 import 'assets/reset.css'
 import 'swiper/css/swiper.min.css'
+// import
 function App() {
   // console.log(TabBarExample)
   return (
